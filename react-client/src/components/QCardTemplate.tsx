776fabff03cb00e8959a888c405be93100b247e0
Aniket Nayak <aniketn@terpmail.umd.edu>
2024-05-14 19:01:34 -0700

    qcard format 2 prog

--- react-client/src/components/QCardTemplate.tsx
+++ react-client/src/components/QCardTemplate.tsx
@@ -13,12 +13,12 @@ import { Outlet, Link } from "react-router-dom";
 import { Textfit } from 'react-textfit';
 
 export default function QCardTemplate({}) {
-    const [isNotepadOpen, setisNotepadOpen] = useState('0'); // 0 = false
+    const [isNotepadOpen, setisNotepadOpen] = useState(true); // default is true
     const [selectedOption, setselectedOption] = useState('0'); // 0 = none
     const [isFlagged, setisFlagged] = useState(false); // default is false
     const [showTimer, setshowTimer] = useState(true); // default is true
 
-    if (isNotepadOpen == '0') {
+    if (isNotepadOpen == false) {
         return (
             <div className='qcard-bg'>
                 <div className='qcard-top-bar'>
@@ -27,7 +27,7 @@ export default function QCardTemplate({}) {
                             <div id='qcard-back-button'>
                                 <h3><Link to={"/home"}>Exit</Link></h3>
                             </div>
-                            <img src={note}/>
+                            <img src={note} onClick={() => {setisNotepadOpen(true)}}/>
                         </div>
                         <div className='qcard-question-head'>
                             <h1>Q.1</h1>
@@ -80,8 +80,46 @@ export default function QCardTemplate({}) {
         )
     } else {
         return (
-            <div>
-                <h1>hi</h1>
+            <div className='qcard-bg' data-noteshow={true}>
+                <div className='qcard-left-bar'>
+                    <img src={note} onClick={() => {setisNotepadOpen(false)}}/>
+                    <img src={unflag} id="unflag" data-flag={isFlagged.toString()} onClick={() => {setisFlagged(true)}}/>
+                    <img src={flag} id="flag" data-flag={isFlagged.toString()} onClick={() => {setisFlagged(false)}}/>
+                </div>
+                <div className='qcard-vert-line'></div>
+                <div>
+                    <div className='qcard-main-content' data-noteshow={true}>   
+                        <img src={share} id="share" data-noteshow={true}/> 
+                        <div className='qcard-text' data-noteshow={true}>
+                            <h2>Maria is staying at a hotel that charges $99.95 per night plus tax for a room. A tax of 8% is applied to the room rate, and an additional onetime untaxed fee of $5.00 is charged by the hotel. Which of the following represents Maria’s total charge, in dollars, for staying x nights?</h2>
+                        </div>
+                        <div className='qcard-mid-bar'><div></div></div>
+                        <div className='qcard-choices' data-noteshow={true}>
+                            <div className='qcard-answer-choice' id="choice1" data-selected={selectedOption} onClick={() => {setselectedOption('1')}}>
+                                <div id="qcard-letter"><h2><span>A</span></h2></div>
+                                <div className='qcard-choice-cont'><h2>(99.95 + 0.08x) + 5</h2></div>
+                            </div>
+
+                            <div className='qcard-answer-choice' id="choice2" data-selected={selectedOption} onClick={() => {setselectedOption('2')}}>
+                                <div id="qcard-letter"><h2><span>B</span></h2></div>
+                                <div className='qcard-choice-cont'><h2>Maria is staying at a hotel that charges $99.95 per night plus tax for a room. A tax of 8% i</h2></div>
+                            </div>
+
+                            <div className='qcard-answer-choice' id="choice3" data-selected={selectedOption} onClick={() => {setselectedOption('3')}}>
+                                <div id="qcard-letter"><h2><span>C</span></h2></div>
+                                <div className='qcard-choice-cont'><h2>(99.95 + 0.08x) + 5</h2></div>
+                            </div>
+
+                            <div className='qcard-answer-choice' id="choice4" data-selected={selectedOption} onClick={() => {setselectedOption('4')}}>
+                                <div id="qcard-letter"><h2><span>D</span></h2></div>
+                                <div className='qcard-choice-cont'><h2>hat charges $99.95 per night plus tax for a room.</h2></div>
+                            </div>
+                        </div>
+                        <div className='qcard-bottom-icons' data-noteshow={true}>
+                            <img src={arrow} id="arrow" data-show={selectedOption}/>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
