53e1c59ccdd28e2df0634ba764f297ca13dbb282
Aniket Nayak <aniketn@terpmail.umd.edu>
2024-06-05 12:05:25 -0700

    homepage base done

--- react-client/src/components/HomeStuff/YourQuestionList.tsx
+++ react-client/src/components/HomeStuff/YourQuestionList.tsx
@@ -57,12 +57,53 @@ const QuestionListComponent = () => {
             <div className='mb-4'>
                 <h1 className='text-3xl'>Problem List</h1>
             </div>
-            <div className=''>
-                <div className="flex mb-4 space-x-4">
+            <div className="w-full flex mb-6 justify-between">
+                <div className='flex space-x-4'>
                     <h1>Filters:</h1>
-                    <button>Difficulty</button>
-
+                    <button className='bg-[#F2F3F4] text-[#555555] rounded-[12px] px-2 py-0.5 drop-shadow-[0_2px_0px_rgba(140,147,153,.3)] flex items-center justify-center gap-2 relative bottom-[2px]'>
+                        Difficulty
+                        <svg height="10" viewBox="-2.5 -5 75 60" preserveAspectRatio="none">
+                            <path d="M0,0 l35,50 l35,-50" fill="none" stroke="#555555" stroke-linecap="round" stroke-width="10" />
+                        </svg>
+                    </button>
                 </div>
+                <div>
+                    <button className='bg-[#3483F9] text-white font-bold rounded-[12px] px-2 py-0.5 drop-shadow-[0_2px_0px_rgba(9,85,199,1)] relative bottom-[2px]'>Create Custom Set</button>
+                </div>
+            </div>
+
+            <div className='w-full h-[2px] bg-[#040033] thisisabarBTW'></div>
+            {filteredQuestions?.map(question => (
+                <div className=''>
+                    <div key={question._id}
+                        className={`p-4 flex items-center justify-between cursor-pointer hover:bg-gray-200 transition-colors ${question.completed ? 'bg-green-100' : ''}`}
+                        onClick={() => handleQuestionClick(question._id)}>
+                        <div className='flex items-center gap-4'>
+                            <div className="text-lg font-medium">{question.title}</div>
+                            <div className="text-sm text-[#555555]">{question.category}</div> {/* TODO: for now category but should be subtopic */}
+                        </div>
+                        <div className='text-lg font-medium question-diff' data-diff={question.difficulty.toLowerCase()}>{question.difficulty}</div>
+                    </div>
+                    <div className='w-full h-[2px] bg-[#040033] thisisabarBTW'></div>
+                </div>
+            ))}
+            
+            <div className="flex justify-between mt-4">
+                <button
+                    className="px-4 py-2 bg-gray-300 rounded-md disabled:opacity-50"
+                    disabled={currentPage === 1}
+                    onClick={() => handlePageChange(currentPage - 1)}
+                >
+                    Previous
+                </button>
+                <div className="text-gray-700">Page {currentPage} of {totalPages}</div>
+                <button
+                    className="px-4 py-2 bg-gray-300 rounded-md disabled:opacity-50"
+                    disabled={currentPage === totalPages}
+                    onClick={() => handlePageChange(currentPage + 1)}
+                >
+                    Next
+                </button>
             </div>
         </div>
     );
